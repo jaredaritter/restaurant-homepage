d794cf7875768df5e95ce6d71f198676a6f336e2
jared <38332939+jaredaritter@users.noreply.github.com>
2020-06-26 10:44:18 -0400

    Small content update for homepage

--- src/homepage.js
+++ src/homepage.js
@@ -7,7 +7,7 @@ function homepage() {
     img: [Tacos, 'homepage-img'],
     p: [
       "You want tacos? We got tacos. We have the best tacos you have ever put in your face. Don't believe us? Just ask Dave. *Listens intently*. Yeah Dave just yelled from the back that we definitely have the best tacos that you have ever put in your face. Sorry if you didn't hear him, he's pretty far away.",
-      "We also have other items on the menu for your enjoyment. We have the staples like burritos and quesadillas, along with some of the classics like lechon asado and pollo con mole. We're going all over South and Central America here with a quick jaunt through the islands as well because it's all delicious. Come join us!",
+      "We also have other items on the menu for your enjoyment. We have the staples like burritos and quesadillas, along with some of the classics like lechon asado and pollo con mole. We're going all over South and Central America here with a quick jaunt through the islands as well because it's all delicious. Come join us and put some food in your face!",
     ],
   };
 
